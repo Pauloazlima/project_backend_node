4e22ae0f11bccc85df9b97a1bb203011465649e2
Pauloazlima <Paulo.azlima3008@gmail.com>
2024-10-07 23:00:28 -0300

    gitIgnoreOld_productManager

--- src/modules/teste.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-const path = 'src\files\products.txt'
-
-async getProducts () {
-      const data = await fs.promises.readFile(path, 'utf-8');
-}
-
-console.log(getProducts);
