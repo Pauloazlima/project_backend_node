9e42bf67b0f9fb5c51249457c4d74a0b7a961ada
Pauloazlima <Paulo.azlima3008@gmail.com>
2024-11-04 18:44:43 -0300

    ajusteModelMessageseEChatmesage.handlebars

--- src/models/messages.model.js
+++ src/models/messages.model.js
@@ -1,13 +1,19 @@
 const mongoose = require('mongoose');
 
 const messagesCollection = 'messages';
-
 const messagesSchema = new mongoose.Schema({
   user: {
     type: String,
     required: true
   },
-  message: String
-})
+  message: {
+    type: String,
+    required: true
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now
+  }
+});
 
 module.exports = mongoose.model(messagesCollection, messagesSchema);
