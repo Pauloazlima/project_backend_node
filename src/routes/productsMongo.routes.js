9d33877f0584c61ef7ad68b07a1d1817831d3091
Pauloazlima <Paulo.azlima3008@gmail.com>
2024-11-03 22:46:00 -0300

    postCriacaoNaoFuncionando

--- src/routes/productsMongo.routes.js
+++ src/routes/productsMongo.routes.js
@@ -14,5 +14,21 @@ router.get('/mongo', async (req,res) => {
 }
 })
 
+router.post('/add', async (req,res) => {
+  try {
+      let  {title, description, price, thumbnail, code, stock, category} = req.body;
+      if (!title, !description, !price, !thumbnail, !code, !stock, !category){
+        return res.status(500).send({result: 'Todos os campos são obrigatórios'})
+      }
+      let inputInfo = await productsModel.create(body)
+      res.status(201).json({result: 'success', payload: inputInfo})
+  } catch (error){
+    console.log('não foi possivel adicionar as informacoes do produto');
+    res.status(500).send({result: 'erro', error: 'Erro ao adicionar'})
+    
+
+  }
+})
+
 
 module.exports = router
