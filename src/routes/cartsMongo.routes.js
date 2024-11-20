3c0121aee627c44cc572dd76d3cb5353d78cadf3
Pauloazlima <Paulo.azlima3008@gmail.com>
2024-11-20 13:59:40 -0300

    rotasCartsTudoOK

--- src/routes/cartsMongo.routes.js
+++ src/routes/cartsMongo.routes.js
@@ -177,6 +177,27 @@ router.get('/paginate/:cid', async (req, res) => {
   }
 });
 
+router.delete('/deleteAllProducts/:cid', async (req, res) => {
+  try {
+    const { cid } = req.params;
+
+    const cart = await cartsModel.findById(cid);
+    if (!cart) {
+      return res.status(404).json({ error: 'Carrinho não encontrado' });
+    }
+
+    cart.products = [];
+    await cart.save();
+
+    res.status(200).json({ message: 'Todos os produtos foram removidos do carrinho', payload: cart });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro ao remover os produtos do carrinho' });
+  }
+});
+
+
+
 
 
 
