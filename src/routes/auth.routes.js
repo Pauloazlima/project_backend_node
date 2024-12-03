57fec174f07ece5418aafd05c5da184056067a12
Pauloazlima <paulo.azlima3008@gmail.com>
2024-12-03 10:50:31 -0300

    funcionando

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -5,45 +5,53 @@ const passport = require('passport');
 const router = express.Router();
 
 
-// router.post('/register', async (req, res) => {
-//     console.log('Body recebido:', req.body);
-//     const { name, email, password } = req.body;
+router.post('/register', async (req, res) => {
+    console.log('Body recebido:', req.body);
+    const { name, email, password } = req.body;
+
+    const userAlreadyExists = await User.findOne({email});
+
+    if (userAlreadyExists) {
+      return res.status(400).send('User existente!');
+    }
+
+    if (!name || !email || !password) {
+      return res.status(400).send('Todos os campos são obrigatórios.');
+    }
   
-//     if (!name || !email || !password) {
-//       return res.status(400).send('Todos os campos são obrigatórios.');
-//     }
-  
-//     try {
-//       const newUser = new User({ name, email, password });
-//       await newUser.save();
-//       res.redirect('/login');
-//     } catch (error) {
-//       console.error('Erro ao registrar usuário:', error);
-//       res.status(500).send('Erro no servidor');
-//     }
-//   })
+    try {
+      const newUser = new User({ name, email, password });
+      await newUser.save();
+      res.redirect('/login');
+    } catch (error) {
+      console.error('Erro ao registrar usuário:', error);
+      res.status(500).send('Erro no servidor');
+    }
+  })
 
 
-// router.post('/login', async (req, res) => {
-//     const { email, password } = req.body;
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
 
-//     try {
-//         const user = await User.findOne({ email });
-//         if (!user) {
-//             return res.status(401).json({ message: 'Credenciais inválidas' });
-//         }
+    
+
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(401).json({ message: 'Credenciais inválidas' });
+        }
 
         
-//         if (user.password !== password) {
-//             return res.status(401).json({ message: 'Credenciais inválidas' });
-//         }
+        if (user.password !== password) {
+            return res.status(401).json({ message: 'Credenciais inválidas' });
+        }
 
-//         return res.redirect('/products/mongo/products/paginate');
-//     } catch (error) {
-//         console.error('Erro durante o login:', error);
-//         return res.status(500).json({ message: 'Erro interno do servidor' });
-//     }
-// });
+        return res.redirect('/products/mongo/products/paginate');
+    } catch (error) {
+        console.error('Erro durante o login:', error);
+        return res.status(500).json({ message: 'Erro interno do servidor' });
+    }
+});
 
 // router.post('/logout', (req, res) => {
 //   req.session.destroy((err) => {
@@ -54,31 +62,23 @@ const router = express.Router();
 //   });
 // });
 
-router.post(
-  '/register',
-  passport.authenticate('register', {
-    successRedirect: '/login',
-    failureRedirect: '/register',
-    failureFlash: true,
-  })
-);
+// router.post(
+//   '/register',
+//   passport.authenticate('register', {
+//     successRedirect: '/login',
+//     failureRedirect: '/register',
+//     failureFlash: true,
+//     session: false
+//   })
+// );
 
 
-router.post(
-  '/login',
-  passport.authenticate('login', {
-    successRedirect: '/products',
-    failureRedirect: '/login',
-    failureFlash: true,
-  })
-);
-
-router.post('/logout', (req, res) => {
-  req.logout((err) => {
-    if (err) return res.status(500).json({ error: 'Erro ao fazer logout' });
-    res.redirect('/login');
-  });
-});
+// router.post('/logout', (req, res) => {
+//   req.logout((err) => {
+//     if (err) return res.status(500).json({ error: 'Erro ao fazer logout' });
+//     res.redirect('/login');
+//   });
+// });
 
 // GitHub Login - nao esquecer do session: false
 router.get('/github', passport.authenticate('github'));
@@ -87,7 +87,7 @@ router.get(
   '/github/callback',
   passport.authenticate('github', {
     session: false,
-    successRedirect: '/realtimeproducts',
+    successRedirect: '/products/mongo/products/paginate',
     failureRedirect: '/login',
   })
 );
